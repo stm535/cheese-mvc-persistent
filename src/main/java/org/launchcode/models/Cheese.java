928fffb04c2c935e3e74695792fd2e3ba81aa340
Shannon McCoy <mccoy2524@gmail.com>
2018-01-04 19:56:13 -0600

    Final Project!

--- src/main/java/org/launchcode/models/Cheese.java
+++ src/main/java/org/launchcode/models/Cheese.java
@@ -1,10 +1,9 @@
 package org.launchcode.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.List;
 
 /**
  * Created by LaunchCode
@@ -24,7 +23,11 @@ public class Cheese {
     @Size(min=1, message = "Description must not be empty")
     private String description;
 
-    private CheeseType type;
+    @ManyToOne
+    private Category category;
+
+    @ManyToMany(mappedBy = "cheeses")
+    private List<Menu> menus;
 
     public Cheese(String name, String description) {
         this.name = name;
@@ -45,19 +48,13 @@ public class Cheese {
         this.name = name;
     }
 
-    public String getDescription() {
-        return description;
-    }
+    public String getDescription() { return description; }
 
     public void setDescription(String description) {
         this.description = description;
     }
 
-    public CheeseType getType() {
-        return type;
-    }
+    public Category getCategory() { return category; }
 
-    public void setType(CheeseType type) {
-        this.type = type;
-    }
+    public void setCategory(Category category) { this.category = category; }
 }
